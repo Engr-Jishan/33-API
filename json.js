dbe82045d175fdb3bb7a4b9ec66f8c23e10be749
jisha66 <mydreamplace2020@gmail.com>
2022-09-17 22:53:40 +0600

    second page

---
+++ json.js
@@ -0,0 +1,12 @@
+
+// function(loadClick) {
+//     fetch('https://jsonplaceholder.typicode.com/todos/1')
+//         .then(response => response.json())
+//         .then(json => console.log(json))
+// }
+
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(json => console.log(json))
+}
--- note.js
+++ note.js
@@ -1,1 +1,16 @@
-console.log('API');
+// console.log('API');
+// const user = { id: 11, name: 'Jisan Ahmed', job: 'programmer' };
+// const stringified = JSON.stringify(user);
+// console.log(stringified);
+
+// const shop = {
+//     name: 'Jishan Ahmed',
+//     address: 'Delpara',
+//     profit: ' 5, 000',
+//     products: ['B&H', 'MALBRO', 'GOLDLEAF'],
+//     isExpensive: false
+// }
+
+// const shopStingified = JSON.stringify(shop);
+// console.log(shop);
+// console.log(shopStingified);
