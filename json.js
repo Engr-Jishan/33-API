ecdd64b146c65e543d880214390e01ecb76c504f
jisha66 <mydreamplace2020@gmail.com>
2022-09-18 14:13:02 +0600

    third page

--- json.js
+++ json.js
@@ -8,5 +8,23 @@
 function loadData() {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
         .then(response => response.json())
-        .then(json => console.log(json))
+        .then(json => console.log(json));
+}
+
+function loadUsers() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
+function loadPhotos() {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then(res => res.json())
+        .then(fex => console.log(fex));
+}
+
+function loadAlbums() {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+        .then(res => res.json())
+        .then(albums => console.log(albums));
 }
